f61702689cc25b7067bcf7a821b36d58d874158e
cyber-sprout <sue.ng.pham@gmail.com>
2022-10-30 16:49:55 +0800

    day 15 and 16

---
+++ Day 16/main.py
@@ -0,0 +1,31 @@
+# Timmy the Turtle
+
+# import another_module
+# print(another_module.another_variable)
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkSeaGreen")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+##############################
+
+# https://code.visualstudio.com/docs/python/environmentspwd
+
+# create new venv
+# python3 -m venv
+
+# Activate new venv
+# source .day16/bin/activate
+
+##############################
+
+# Pokemon thing
+print("hello")
+
